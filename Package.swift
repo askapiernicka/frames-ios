ec1dd08e31ed147fd3d3199f9a30e6fd9d7a1a8e
askapiernicka <joapiernicka@gmail.com>
2020-11-05 14:10:26 +0100

    Update Package.swift
    fix resolving SPM dependencies

--- Package.swift
+++ Package.swift
@@ -11,17 +11,17 @@ let package = Package(
             name: "FramesIos",
             targets: ["FramesIos"]),
         ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0")
-    ],
+//     dependencies: [
+//         // Dependencies declare other packages that this package depends on.
+//         // .package(url: /* package url */, from: "1.0.0"),
+//         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0")
+//     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
        .target(
            name: "FramesIos",
-           dependencies: ["Alamofire"],
+//            dependencies: ["Alamofire"],
            path: "Source"),
 //        .testTarget(
 //            name: "FramesIosTests",
